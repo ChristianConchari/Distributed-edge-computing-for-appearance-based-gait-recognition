c7f73709ae98d146638e4e03e1b847a3d143c7bb
Christian Conchari <chrisconchari@gmail.com>
2024-02-15 15:38:27 -0400

    modify create_dir function

--- Offline-processing/src/create_dir.py
+++ Offline-processing/src/create_dir.py
@@ -3,45 +3,29 @@ This module contains the create_dir function, which creates a directory at the s
 """
 from os import makedirs
 from shutil import rmtree
-from argparse import ArgumentParser
 
 def create_dir(folder: str, force: bool = True, verbose: bool = False) -> None:
     """
-    Creates a directory at the specified path.
+    Create a directory if it doesn't exist at the specified path.
 
     Parameters:
     folder (str): The path of the directory to be created.
-    force (bool): If True, deletes the directory if it already exists before creating a new one. Default is True.
-    verbose (bool): If True, prints additional information during the directory creation process. Default is False.
+    force (bool): If True, deletes the directory if it already 
+        exists before creating a new one. Default is True.
+    verbose (bool): If True, prints additional information 
+        during the directory creation process. Default is False.
     """
     try:
         makedirs(folder)
-        
-    except FileExistsError as e:
         if verbose:
-            print(e)
+            print(f'Directory {folder} created succesfully.')
+    except FileExistsError as e:
         if force:
+            if verbose:
+                print(e)
             rmtree(folder)
             makedirs(folder)
-
-def main() -> None:
-    """
-    Main function that creates a directory based on the provided path.
-
-    Args:
-        None
-
-    Returns:
-        None
-    """
-    parser = ArgumentParser(description="Create a directory.")
-    parser.add_argument("path", help="The path of the directory to be created.")
-    parser.add_argument("--force", action='store_true', help="If set, deletes the directory if it already exists before creating a new one.")
-    parser.add_argument("--verbose", action='store_true', help="If set, prints additional information during the directory creation process.")
-    args = parser.parse_args()
-    
-    create_dir(args.path, force=args.force, verbose=args.verbose)
-
-if __name__ == '__main__':
-    main()
-    
+        else:
+            if verbose:
+                print(f'Directory {folder} already exists.')
+        
